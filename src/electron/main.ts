9597b797dbba52aada8b4b99e275faf7d447c981
miniaturity <miniaturity2.o@gmail.com>
2025-06-30 20:45:25 -0400

    Static Data Gathering

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,23 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
 import path from 'path';
+import { isDev } from './util.js';
+import { getPreloadPath } from "./pathResolver.js";
+import { getStaticData, pollResources } from "./resourceManager.js";
 
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            preload: getPreloadPath()
+        }
+    });
+    if (isDev()) {
+        mainWindow.loadURL('http://localhost:5173');
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+    }
+
+    pollResources(mainWindow);
+    ipcMain.handle("getStaticData", (event)=>{
+        return getStaticData();
+    })
 });
